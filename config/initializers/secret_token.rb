6e5d670f911877c82c66a172bbd6a8e473654b28
mwi <mwi@youngsolutions.ch>
2014-03-21 17:55:31 +0100

    initial setup of rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BfhOs2Toolchain::Application.config.secret_key_base = 'fc86c58d34fae8d49ef777299ee6336886d7cc3122cb617c832ce6d6921e49d4439e0fdd27a37be95f8e54f89a33c0b794e7f23f2b48591ee63be5fa3b3ba190'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BfhOs2Toolchain::Application.config.session_store :cookie_store, key: '_bfh-os2-toolchain_session'
